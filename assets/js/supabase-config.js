62494f6e60cfe8485925ca6b2cfdad311618153c
riteshgavade53-blip <riteshgavade53@gmail.com>
2026-03-04 10:35:47 +0530

    Update supabase-config.js

--- assets/js/supabase-config.js
+++ assets/js/supabase-config.js
@@ -1,8 +1,8 @@
 // Fill these values from your Supabase project settings.
 window.SUPABASE_CONFIG = {
-url: '',
-anonKey: '',
+url: 'https://hzkrhneewxocqtvngpaa.supabase.co',
+anonKey: 'sb_publishable_lClDARHVv4fxNfO07X-k2A__BcsVDVt',
 // Add one or more admin emails created in Supabase Auth.
 // Leave empty array to allow any signed-in user as admin (not recommended).
-adminEmails: ['']
+adminEmails: ['riteshgavade53@gmail.com']
 };
